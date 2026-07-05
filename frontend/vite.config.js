edb35f2dd393b330b0b80bf61bbefeb45d17d712
Petar <shaker4etoooo@gmail.com>
2026-07-05 12:41:27 +0300

    Delete react.svg cruft

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: [path.resolve(__dirname, '..')], // allow importing from repo root
-    },
-  },
 })
